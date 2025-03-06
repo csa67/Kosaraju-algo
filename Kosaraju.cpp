dfa27422edb9f6944eb19885c4727df874458a5d
csa67 <saiaarathy@gmail.com>
2025-03-06 12:44:40 -0500

    Added all functions

--- Kosaraju.cpp
+++ Kosaraju.cpp
@@ -5,6 +5,10 @@
 
 using namespace std;
 
+void dfsFillStack(int node, vector<vector<int>>& adjList, vector<bool>& visited, stack<int>& finishStack);
+void dfsTranspose(int node, vector<vector<int>>& adjList, vector<bool>& visited, vector<int>& scc_nodes);
+vector<vector<int>> getTransposeGraph(vector<vector<int>>& adjList, int V);
+
 void Kosaraju(vector<vector<int>>& adjList, int V){
     
     vector<bool> visited(V, false);
@@ -17,7 +21,40 @@ void Kosaraju(vector<vector<int>>& adjList, int V){
         }
     }
 
-    
+    //Step2: Transpose the whole graph
+    vector<vector<int>> transposedGraph = getTransposeGraph(adjList, V);
+
+    for(const auto& edge:transposedGraph){
+        for(int node:edge){
+            cout<<node<<" ";
+        }
+        cout<<endl;
+    }
+
+    fill(visited.begin(),visited.end(),false);
+    int scc = 0;
+    vector<vector<int>> scc_list;
+
+    //Step 3: Perform DFS based on the finish stack on the transposed graph
+    while(!finishStack.empty()){
+        int node = finishStack.top();
+        finishStack.pop();
+
+        if(!visited[node]){
+            vector<int> scc_nodes;
+            dfsTranspose(node,transposedGraph,visited,scc_nodes);
+            scc++;
+            scc_list.push_back(scc_nodes);
+        }
+    }
+
+    cout<< "Total no:of strongly connected components: " << scc << endl;
+    for(const auto& scc:scc_list){
+        for(int node:scc){
+            cout<<node<<" ";
+        }
+        cout<<endl;
+    }
 }
 
 void dfsFillStack(int node, vector<vector<int>>& adjList, vector<bool>& visited, stack<int>& finishStack){
@@ -32,13 +69,47 @@ void dfsFillStack(int node, vector<vector<int>>& adjList, vector<bool>& visited,
     finishStack.push(node);
 }
 
-void dfsTranspose(int node, vector<vector<int>>& adjList, vector<bool>& visited){
+void dfsTranspose(int node, vector<vector<int>>& adjList, vector<bool>& visited, vector<int>& scc_nodes){
 
     visited[node] = true;
+    scc_nodes.push_back(node);
+
     vector<int>& neighborsList = adjList[node];
     for(int neighbor:neighborsList){
         if(!visited[neighbor]){
-            dfsTranspose(neighbor,adjList,visited);
+            dfsTranspose(neighbor,adjList,visited,scc_nodes);
         }
     }
+}
+
+vector<vector<int>> getTransposeGraph(vector<vector<int>>& adjList, int V){
+    vector<vector<int>> new_graph(V);
+    for(int u=0; u<V;u++){
+        for(int v: adjList[u]){
+            new_graph[v].push_back(u);
+        }
+    }
+
+    return new_graph;
+}
+
+int main(){
+    int V,E;
+
+    cout << "Enter number of vertices (V): ";
+    cin >> V;
+    cout << "Enter number of edges (E): ";
+    cin >> E;
+
+    vector<vector<int>> adjList(V);
+    cout << "Enter the edges (u v):\n";
+    for (int i = 0; i < E; i++) {
+        int u, v;
+        cin >> u >> v;
+        adjList[u].push_back(v);  // Add edge u -> v
+    }
+
+    Kosaraju(adjList, V);
+
+    return 0;
 }
